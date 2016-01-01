a34e82892378ade55aceee417f66d1c90a5ee333
deathcap <deathcap@gmx.co.uk>
2015-12-31 20:03:53 -0800

    Add array/non-array object checking

--- decorum.js
+++ decorum.js
@@ -7,16 +7,30 @@ module.exports = function(obj, iface) {
     var expected = iface[name];
 
     if (!(name in obj)) {
-      throw new Error('object is missing property: '+name+': '+obj);
+      throw new Error('object is missing property: "'+name+'": '+obj);
     }
 
     var actual = obj[name];
 
-    // TODO: arrays ([] is object)
-    // TODO: allow undefined? typeof undefined === 'undefined'
     if (typeof expected !== typeof actual) {
-      throw new Error('object property '+name+' is of type '+typeof actual+', but expected '+typeof expected);
+      throw new Error('object property "'+name+'" expected '+typeof expected+', but got '+typeof actual);
     }
+
+    // {} and [] both have typeof 'object', so differentiate them here
+    if (Array.isArray(expected) !== Array.isArray(actual)) {
+        throw new Error('object property "'+name+'" expected '+
+          (Array.isArray(expected) ? 'array ' : 'non-array ') +
+          typeof(expected) +
+          ', but got '+
+          (Array.isArray(actual) ? 'array ' : 'non-array ') +
+          typeof(actual));
+    }
+
+    // TODO: other object subtypes? could get arbitrarily complex, recursive?
+    // TODO: integers vs floats?
+    // TODO: allow undefined? typeof undefined === 'undefined'
+
+
   }
 
   return true;
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var test = require('tape');
 
 var conforms = require('./');
 
-test(function(t) {
+test('duck', function(t) {
 
   var duck = {
     quack: function() {},
@@ -19,6 +19,46 @@ test(function(t) {
   });
 
   t.equal(conforms({quack: function() { console.log('quack'); }}, duck), true);
+
+  t.end();
+});
+
+// TODO: check conforms(, non-object)
+
+test('group', function(t) {
+
+  var group = {
+    gid: 100,
+    members: [],
+    settings: {},
+  };
+
+  t.throws(function() {
+    conforms({}, group);
+  });
+
+  t.throws(function() {
+    conforms({gid: 42, members: 'wrong'}, group);
+  });
+
+  t.equal(conforms({
+    gid: 43,
+    members: [1,2,3],
+    settings: {},
+  }, group), true);
+
+  t.equal(conforms({ gid: 0, members: [], settings: {}, }, group), true);
+
+
+  // given object, expected array
+  t.throws(function() {
+    conforms({gid: 44, members: {}, settings: {}}, group);
+  });
+
+  // given array, expected object
+  t.throws(function() {
+    conforms({gid: 45, members: [], settings: []}, group);
+  });
 
   t.end();
 });
