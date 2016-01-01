6ad629ebc9405e56fefb1aa89a0e8732e3ba6861
deathcap <deathcap@gmx.co.uk>
2015-12-31 19:39:09 -0800

    Check for matching object property names and types with typeof(); use interface directly removing createInterface

--- decorum.js
+++ decorum.js
@@ -1,16 +1,30 @@
 'use strict';
 
-var createInterface = function(obj) { // can't use 'interface', reserved JavaScript word :(
-};
-
 var conforms = function(obj, iface) {
+  var keys = Object.keys(iface);
+  for (var i = 0; i < keys.length; ++i) {
+    var name = keys[i];
+    var expected = iface[name];
+
+    if (!(name in obj)) {
+      throw new Error('object is missing property: '+name+': '+obj);
+    }
+
+    var actual = obj[name];
+
+    // TODO: arrays ([] is object)
+    // TODO: allow undefined? typeof undefined === 'undefined'
+    if (typeof expected !== typeof actual) {
+      throw new Error('object property '+name+' is of type '+typeof actual+', but expected '+typeof expected);
+    }
+  }
 };
 
-var duck = createInterface({
+var duck = {
   quack: function() {},
-});
+};
 
-console.log(conforms({}, duck));
+//console.log(conforms({}, duck));
 console.log(conforms({quack: 1}, duck));
 console.log(conforms({quack: function() { console.log('quack'); }}, duck));
 
